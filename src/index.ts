1616fd48fac5daddd33042ac0deb9d2b1cacdbbe
Aleksey Lutsenko <aslutsenko.github@gmail.com>
2021-03-16 18:49:21 +0300

    Added logger

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import "reflect-metadata";
 import './bootstrap'
-import { createConnection } from "typeorm";
+import { createConnection, FileLogger } from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { App } from "./types/app";
 import { Service } from "./enum/service";
+import { LogLevel } from "./enum/log-level";
 
 const config = container.resolve<App.Config.Service>(Service.Config)
+const logger = container.resolve<App.Logger.Service>(Service.Logger)
 
 createConnection().then(async connection => {
     const { Routes } = require('./routes')
@@ -46,7 +48,7 @@ createConnection().then(async connection => {
 
     // start express server
     app.listen(config.port, () => {
-        console.log(`Application running on port: ${config.port}`)
+        logger.log(LogLevel.Info, `Application running on port: ${config.port}`)
     });
 
     // insert new users for test
--- src/types/app.ts
+++ src/types/app.ts
@@ -1,10 +1,25 @@
 import { NextFunction, Request, Response } from "express";
+import { LogLevel } from "../enum/log-level";
 
 export declare namespace App {
   namespace Config {
 
     interface Service {
       readonly port: number;
+      readonly logLevel: LogLevel;
+    }
+  }
+
+  namespace Logger {
+    interface Service {
+      log(level: LogLevel, message: string): void;
+      error(message: string): void
+      warn(message: string): void
+      info(message: string): void
+      http(message: string): void
+      verbose(message: string): void
+      debug(message: string): void
+      silly(message: string): void
     }
   }
 
